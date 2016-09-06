a3886173ba6b1744978b36fe344fe79131bd66d0
Paco Navarro <paco-navarro@users.noreply.github.com>
2016-09-06 14:58:58 +0200

    Cambiar las credenciales

--- server/app.js
+++ server/app.js
@@ -54,6 +54,4 @@ app.listen(server_port, server_ip_address, function() {
   console.log('App listening on port ' + server_port);
 });
 
-
-itemController.watchPrices();
 cron.schedule('* */6 * * *', itemController.watchPrices);
